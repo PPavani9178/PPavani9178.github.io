5980eea049b48459bfed783dde111e2b9b5404da
Peketi <pavani1642002@gmail.com>
2024-05-12 19:53:49 +0530

    added1

---
+++ src/components/Portfolio/project.js
@@ -0,0 +1,32 @@
+import marvel from "../../img/marvel.png"
+import gadget_galaxy from "../../img/gadget_galaxy.png"
+import speedup from "../../img/speedup.png";
+
+export const project = [
+  {
+    img: speedup,
+    title: "SPEEDUP",
+    desc: "The SPEEDUP Website is a platform designed to provide information and resources for users interested in electric transportation solutions. It aims to promote awareness and adoption of electric vehicles and related technology.",
+    tech: "Html, CSS, JAVASCRIPT,Json server",
+    git: "https://github.com/PPavani9178/CW_js",
+    deploy: "https://661c34ac0093cdaefbed2803--unrivaled-profiterole-acd4e9.netlify.app/"
+  },
+  {
+    img: marvel,
+    title: "Marvel Front Website",
+    desc: "The Marvel Front Website is an application designed to provide users with information about Marvel comics, characters, and related content. It serves as a platform for fans to explore their favorite Marvel stories, characters, and events.",
+    tech: "Html,CSS,Bootstrap",
+    git: "https://github.com/sathwikreddy122/CW_Marvel",
+    deploy: "https://clever-alpaca-8b5e93.netlify.app/"
+  },
+
+  {
+    img: gadget_galaxy,
+    title: "Reliance-Digital Clone",
+    desc: "Gadget Galaxy, an electronic E-commerce platform, offers a diverse selection of gadgets. Crafted by a team of 5, the project prioritizes seamless user experiences. Built with HTML, CSS, and Bootstrap, it delivers a streamlined shopping interface for the latest electronics.",
+    tech: "Html,Bootstrap, CSS",
+    git: "https://github.com/Anujkumar960/Gadget-Galaxy",
+    deploy: "https://gadget-galaxy-project.netlify.app/"
+  }
+  
+];
